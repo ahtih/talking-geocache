9325516840de3951210d3a25ccb10e2419c9f4af
Ahti Heinla <ahti@ahti.bluemoon.ee>
2018-04-10 21:24:13 +0300

    Added prepare_sd_card.py

---
+++ prepare_sd_card.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import sys,math,wave
+
+def bitreverse(value,nr_of_bits):
+	result=0
+	for i in range(nr_of_bits):
+		result<<=1
+		result+=(value & 1)
+		value>>=1
+	return result
+
+wave_file=wave.open(sys.argv[1],'r')
+output_file=open(sys.argv[2],'wb')
+
+nr_of_frames=wave_file.getnframes()
+audio_data_string=wave_file.readframes(nr_of_frames)
+
+for i in range(nr_of_frames):
+	value=ord(audio_data_string[i*2]) + 256*ord(audio_data_string[i*2+1])
+	if value >= 2**15:
+		value-=2**16
+
+	output_value=bitreverse((value >> 8) & 255,8)
+	# output_value=bitreverse((20 if (i % 128) >= 64 else -20) if (i % (2*41667)) < 41667 else 0,8)
+	# output_value=bitreverse(int(40 * math.sin((i % 128) * 2.0 * math.pi / 128.0)),8)
+
+	output_file.write(chr(output_value))
+
+	# print '%+d %02x' % (value,output_value)
+
+wave_file.close()
+output_file.close()
