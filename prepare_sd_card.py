4760c6f3238e7e3e127d31639d41f233fefe711f
Ahti Heinla <ahti@ahti.bluemoon.ee>
2018-04-21 20:08:35 +0300

    Added interaction script file parsing

--- prepare_sd_card.py
+++ prepare_sd_card.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import sys,math,wave,crc16
+import sys,math,struct,wave,crc16
 
 def bitreverse(value,nr_of_bits):
 	result=0
@@ -61,62 +61,130 @@ if len(sys.argv) < 1+2:
 
 	exit(0)
 
-wave_file=wave.open(sys.argv[1],'r')
-output_file=open(sys.argv[2],'wb')
+def process_audio_file(fname):
+	global output_audio_data
 
-nr_of_frames=wave_file.getnframes()
-audio_data_string=wave_file.readframes(nr_of_frames)
+	wave_file=wave.open(fname,'r')
 
-unmodified_block_str=''
-residual_delta=0
+	nr_of_frames=wave_file.getnframes()
+	audio_data_string=wave_file.readframes(nr_of_frames)
 
-for i in range(nr_of_frames):
-	value=ord(audio_data_string[i*2]) + 256*ord(audio_data_string[i*2+1])
-	if value >= 2**15:
-		value-=2**16
+	unmodified_block_str=''
+	residual_delta=0
+	nr_of_samples=0
 
-	# value=((20 if (i % 128) >= 64 else -20) if (i % (2*41667)) < 41667 else 0) * 256
-	# value=int(40 * math.sin((i % 128) * 2.0 * math.pi / 128.0)) * 256
+	for i in range(nr_of_frames):
+		value=ord(audio_data_string[i*2]) + 256*ord(audio_data_string[i*2+1])
+		if value >= 2**15:
+			value-=2**16
 
-	value+=residual_delta
-	output_value=translate_value_to_byte(value)
-	residual_delta=translation_table[output_value] - value
+		# value=((20 if (i % 128) >= 64 else -20) if (i % (2*41667)) < 41667 else 0) * 256
+		# value=int(40 * math.sin((i % 128) * 2.0 * math.pi / 128.0)) * 256
 
-	# print '%+d %02x' % (value,output_value)
+		value+=residual_delta
+		output_value=translate_value_to_byte(value)
+		residual_delta=translation_table[output_value] - value
 
-	unmodified_block_str+=chr(output_value)
+		# print '%+d %02x' % (value,output_value)
 
-	if len(unmodified_block_str) >= 512:
-		best_crc_error=1e6
+		unmodified_block_str+=chr(output_value)
 
-		for idx1 in range(len(unmodified_block_str)-1-2):
-			if not best_crc_error:
-				break
-			block_str1=unmodified_block_str[:idx1] + \
-							chr((ord(unmodified_block_str[idx1  ]) + 1      ) & 255) + \
-							chr((ord(unmodified_block_str[idx1+1]) - 1 + 256) & 255)
-			crc1=crc16.crc16xmodem(block_str1)
+		if len(unmodified_block_str) >= 512:
+			best_crc_error=1e6
 
-			for idx2 in range(idx1 + 2,len(unmodified_block_str)-1):
-				block_str2=unmodified_block_str[(idx1 + 2):idx2] + \
-							chr((ord(unmodified_block_str[idx2  ]) - 1 + 256) & 255) + \
-							chr((ord(unmodified_block_str[idx2+1]) + 1      ) & 255) + \
-							unmodified_block_str[(idx2 + 2):]
+			for idx1 in range(len(unmodified_block_str)-1-2):
+				if not best_crc_error:
+					break
+				block_str1=unmodified_block_str[:idx1] + \
+								chr((ord(unmodified_block_str[idx1  ]) + 1      ) & 255) + \
+								chr((ord(unmodified_block_str[idx1+1]) - 1 + 256) & 255)
+				crc1=crc16.crc16xmodem(block_str1)
 
-				crc_value=crc16.crc16xmodem(block_str2,crc1)
-				crc_error=value_error(output_value,(crc_value & 255)) + \
+				for idx2 in range(idx1 + 2,len(unmodified_block_str)-1):
+					block_str2=unmodified_block_str[(idx1 + 2):idx2] + \
+								chr((ord(unmodified_block_str[idx2  ]) - 1 + 256) & 255) + \
+								chr((ord(unmodified_block_str[idx2+1]) + 1      ) & 255) + \
+								unmodified_block_str[(idx2 + 2):]
+
+					crc_value=crc16.crc16xmodem(block_str2,crc1)
+					crc_error=value_error(output_value,(crc_value & 255)) + \
 																value_error(output_value,(crc_value >> 8))
-				if best_crc_error > crc_error:
-					best_crc_error=crc_error
-					cur_block_str=block_str1 + block_str2
-					# print '%s %s %04x %u' % (str(idx1),str(idx2),crc_value,crc_error)
-					if not best_crc_error:
-						break
+					if best_crc_error > crc_error:
+						best_crc_error=crc_error
+						cur_block_str=block_str1 + block_str2
+						# print '%s %s %04x %u' % (str(idx1),str(idx2),crc_value,crc_error)
+						if not best_crc_error:
+							break
+				# break
 
-		print '%.0f%% %u' % (i * 100 / float(nr_of_frames),best_crc_error)
+			print '%s %.0f%% %u' % (fname,i * 100 / float(nr_of_frames),best_crc_error)
 
-		output_file.write(cur_block_str)
-		unmodified_block_str=''
+			output_audio_data+=cur_block_str
+			nr_of_samples+=len(cur_block_str)
 
-wave_file.close()
+			unmodified_block_str=''
+
+	wave_file.close()
+
+	return ((len(output_audio_data) - nr_of_samples) / 512,nr_of_samples)
+
+interaction_states=[]
+
+for line in open(sys.argv[1],'r'):
+	line=line.rstrip()
+	stripped_line=line.lstrip()
+	if not stripped_line:
+		continue
+
+	fields=line.split()
+
+	if stripped_line == line:
+
+			# label+audio line
+
+		if len(fields) != 2:
+			print 'Error parsing line',line
+			exit(1)
+		interaction_states.append(list(fields))
+	else:
+			# knocks line
+
+		if len(fields) != 1 or not interaction_states:
+			print 'Error parsing line',line
+			exit(1)
+		interaction_states[-1].append(fields[0])
+
+script_blocks=(len(interaction_states) * (4+4+9) + 512-1) / 512
+output_audio_data=''
+output_script_data=''
+
+for state in interaction_states:
+	audio_params=process_audio_file(state[1])
+
+	state_output_data=struct.pack('<II',audio_params[0] + script_blocks,audio_params[1])
+
+	next_state_idx=255
+	for i in range(9):
+		if 2+i < len(state):
+			label_name=state[2+i]
+			for idx,s in enumerate(interaction_states):
+				if s[0] == label_name:
+					next_state_idx=idx
+					break
+			else:
+				print "Jump target label '%s' not found" % (label_name,)
+				exit(1)
+		state_output_data+=chr(next_state_idx)
+
+	output_script_data+=state_output_data
+	for c in state_output_data:
+		print '%02x' % (ord(c),),
+	print
+
+while len(output_script_data) < script_blocks*512:
+	output_script_data+=chr(0)
+
+output_file=open(sys.argv[2],'wb')
+output_file.write(output_script_data)
+output_file.write(output_audio_data)
 output_file.close()
