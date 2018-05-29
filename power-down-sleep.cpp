e6cb3e445d8265275ac126243519b64f7e9a3f0e
Ahti Heinla <ahti@ahti.bluemoon.ee>
2018-05-30 01:36:52 +0300

    Disabled several peripherals in setup()

--- power-down-sleep.cpp
+++ power-down-sleep.cpp
@@ -16,10 +16,16 @@ typedef signed long slong;
 void setup(void)
 {
 	// Disable timers and ADC that were enabled by wiring.c init()
-	TCCR0B=0;	// Disable Timer0 (only needed for millis())
-	TCCR1B=0;
-	TCCR2B=0;
-	ADCSRA=0;
+	TCCR0B=0;			// Disable Timer0 (only needed for millis())
+	TCCR1B=0;			// Disable Timer1
+	TCCR2B=0;			// Disable Timer2
+	ADCSRA=0;			// Disable ADC
+
+	ACSR=(1 << ACD);	// Disable Analog Comparator
+	SPCR=0;				// Disable SPI
+	TWCR=0;				// Disable Two-Wire Serial Interface
+	DIDR0=0xff;			// Disable digital input buffers
+	DIDR1=0xff;			// Disable digital input buffers
 	}
 
 ISR(WDT_vect) {}		// Do nothing, just wake up from sleep
